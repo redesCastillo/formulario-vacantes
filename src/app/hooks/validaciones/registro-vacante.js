083e0673049bc51a41d33e9df6e07e48755ad02a
redesCastillo <soporteredes@hotelcastillohuatulco.com>
2025-09-09 11:17:32 -0600

    Delete src directory

--- src/app/hooks/validaciones/registro-vacante.js
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const formSchemaRegistroVacante = z.object({
-  titulo: z.string().min(10, { error: 'Por favor, ingresa el titulo de la vacante' }),
-  descripcion: z.string().min(20, { error: 'Por favor, ingresa una breve descripción del empleo' }),
-  requisitos: z.string().min(7, { error: 'Por favor, ingresa al menos 1 requisito' }),
-  beneficios: z.string().min(7, { error: 'Por favor, ingresa al menos 1 beneficio' }),
-});
